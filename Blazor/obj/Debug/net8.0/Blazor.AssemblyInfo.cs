7cd7ce9445736bbf9011f4325123955fdbd0396c
dkdkamila <dominika.kamila.berg@gmail.com>
2024-01-24 14:55:52 +0100

    utseende

--- Blazor/obj/Debug/net8.0/Blazor.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2545a93b0f596fd270e5f0406566d574bd551cda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12662854a71e5702ccf50d50d2de272dd33b671c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
